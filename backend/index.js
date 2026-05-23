28b63cc57b131a7cfefc1b2af43e7731f3ec4c02
Laura Eraso <laueralor@alum.us.es>
2026-05-23 14:13:03 +0300

    configurar: base del backend con mongodb y estructura frontend con vite

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('El backend de Smart Notes está funcionando perfectamente');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
